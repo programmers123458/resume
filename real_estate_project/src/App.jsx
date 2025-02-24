f41ddeba6d839e3454409c8a775a4aacdfbbfeb6
Diyor <baxacap@gmail.com>
2025-02-24 18:09:40 +0500

    How It Works

--- real_estate_project/src/App.jsx
+++ real_estate_project/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css'
 import Header from './components/Header/Header'
+import HowItWorks from './components/HowItWorks/HowItWorks'
 
 const App = () => {
   return (
     <div>
       <Header />
+      <HowItWorks />
     </div>
   )
 }
