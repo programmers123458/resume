72f659bb1a9d75c0613d9f02cf33ee571b74e102
Diyor <baxacap@gmail.com>
2025-02-24 18:01:19 +0500

    First project -> Header + Hero

---
+++ real_estate_project/src/components/Header/Header.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div>
+      <div id="header-hero-container">
+        <header>
+          <div class="flex container">
+            <a id="logo" href="#">
+              STATED.
+            </a>
+            <nav>
+              <button id="nav-toggle" class="hamburger-menu">
+                <span class="strip"></span>
+                <span class="strip"></span>
+                <span class="strip"></span>
+              </button>
+
+              <ul id="nav-menu">
+                <li>
+                  <a href="#" class="active">
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a href="http://www.google.com" target="_blank">
+                    Properties
+                  </a>
+                </li>
+                <li>
+                  <a href="#">Agents</a>
+                </li>
+                <li>
+                  <a href="#">About</a>
+                </li>
+                <li>
+                  <a href="#">News</a>
+                </li>
+                <li>
+                  <a href="#">Contact</a>
+                </li>
+                <li id="close-flyout">
+                  <span class="fas fa-times"></span>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </header>
+
+        <section id="hero">
+          <div class="fade"></div>
+          <div class="hero-text">
+            <h1>Buy and sell real estate properties</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectectur adipisicing elit.
+              Laboriosam assumenda ea quo cupiditate facere deleniti fuga
+              officia.
+            </p>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
