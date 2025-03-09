344566c10844286ee247379f10a9d5269e43a52e
Diyor <baxacap@gmail.com>
2025-03-09 13:45:56 +0500

    setup

---
+++ app/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ChatApp());
+}
+
+class ChatApp extends StatelessWidget {
+  const ChatApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: Center(child: Text("Welcome to Chat App"),),);
+  }
+}
