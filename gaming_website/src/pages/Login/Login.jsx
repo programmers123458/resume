793e42667227b3a91aceed15b1f323bd0db3deba
Diyor <baxacap@gmail.com>
2025-04-04 14:17:10 +0500

    password generator

--- gaming_website/src/pages/Login/Login.jsx
+++ gaming_website/src/pages/Login/Login.jsx
@@ -1,37 +1,10 @@
-import React from "react";
-import "./Login.css";
+import React from 'react'
+import './Login.css'
 
 const Login = () => {
   return (
-    <div>
-      <div class="container">
-        <form action="#">
-          <h2>Login-form</h2>
-          <div class="input-field">
-            <input type="text" required />
-            <label>Enter email</label>
-          </div>
-          <div class="input-field">
-            <input type="password" required />
-            <label>Enter password</label>
-          </div>
-          <div class="forget">
-            <label for="Save-login">
-              <input type="checkbox" id="Save-login" />
-              <p>Save login information</p>
-            </label>
-            <a href="#">Forgot password?</a>
-          </div>
-          <button type="submit">Log In</button>
-          <div class="Create-account">
-            <p>
-              Don't have an account? <a href="#">Create account</a>
-            </p>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
-};
+    <div>Login</div>
+  )
+}
 
-export default Login;
+export default Login
